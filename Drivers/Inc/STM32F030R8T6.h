dbb92a15c0db31c31b0b1c8d0c734e5ee2ebcfde
SayedFaisalHashimi <sayedfaisalhashimi612@gmail.com>
2026-08-11 15:51:03 +0300

    feat(inc): add APB bus peripheral base address definitions
    Define peripheral base addresses for all APB bus modules on the
    STM32F030R8 in `STM32F030R8T6.h`.
    
    - Add base macros for Timers (TIM1, TIM3, TIM6, TIM7, TIM14-17).
    - Add base macros for Communication Interfaces (USART1/2, SPI1/2, I2C1/2).
    - Add base macros for System & Analog modules (EXTI, SYSCFG, ADC1, PWR).
    - Add base macros for Watchdogs & RTC (IWDG, WWDG, RTC).

--- Drivers/Inc/STM32F030R8T6.h
+++ Drivers/Inc/STM32F030R8T6.h
@@ -39,4 +39,37 @@
  */
 #define RCC_BASE                  (AHB1PERIPH_BASE + 0x00001000U)
 
+/*
+ * Base addresses of peripherals hanging on APB bus (PERIPH_BASE = 0x40000000U)
+ */
+
+/* Timers */
+#define TIM3_BASE                 (APBPERIPH_BASE + 0x00000400U)
+#define TIM6_BASE                 (APBPERIPH_BASE + 0x00001000U)
+#define TIM7_BASE                 (APBPERIPH_BASE + 0x00001400U)
+#define TIM14_BASE                (APBPERIPH_BASE + 0x00002000U)
+#define TIM15_BASE                (APBPERIPH_BASE + 0x00014000U)
+#define TIM16_BASE                (APBPERIPH_BASE + 0x00014400U)
+#define TIM17_BASE                (APBPERIPH_BASE + 0x00014800U)
+#define TIM1_BASE                 (APBPERIPH_BASE + 0x00012C00U)
+
+/* Watchdogs & RTC */
+#define RTC_BASE                  (APBPERIPH_BASE + 0x00002800U)
+#define WWDG_BASE                 (APBPERIPH_BASE + 0x00002C00U)
+#define IWDG_BASE                 (APBPERIPH_BASE + 0x00003000U)
+
+/* Communication Interfaces */
+#define SPI1_BASE                 (APBPERIPH_BASE + 0x00013000U)
+#define SPI2_BASE                 (APBPERIPH_BASE + 0x00003800U)
+#define USART1_BASE               (APBPERIPH_BASE + 0x00013800U)
+#define USART2_BASE               (APBPERIPH_BASE + 0x00004400U)
+#define I2C1_BASE                 (APBPERIPH_BASE + 0x00005400U)
+#define I2C2_BASE                 (APBPERIPH_BASE + 0x00005800U)
+
+/* System & Analog */
+#define PWR_BASE                  (APBPERIPH_BASE + 0x00007000U)
+#define SYSCFG_BASE               (APBPERIPH_BASE + 0x00010000U)
+#define EXTI_BASE                 (APBPERIPH_BASE + 0x00010400U)
+#define ADC1_BASE                 (APBPERIPH_BASE + 0x00012400U)
+
 #endif /* INC_STM32F030R8T6_H_
