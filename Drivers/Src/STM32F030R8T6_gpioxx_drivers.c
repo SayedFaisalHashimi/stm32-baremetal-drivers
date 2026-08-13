5678023dc7a661b12755ec8636a25a659c333d22
SayedFaisalHashimi <sayedfaisalhashimi612@gmail.com>
2026-08-13 16:02:00 +0300

    feat(gpio): implement GPIO peripheral clock control function
    Implement `GPIO_PeriClockControl()` to dynamically enable or disable
    AHB peripheral clock gating for GPIO ports A, B, C, D, and F using
    RCC clock macros.

--- Drivers/Src/STM32F030R8T6_gpioxx_drivers.c
+++ Drivers/Src/STM32F030R8T6_gpioxx_drivers.c
@@ -5,4 +5,32 @@
  *      Author: DELL
  */
 
+#include "STM32F030R8T6_gpioxx_drivers.h"
 
+
+/**
+ * @fn      GPIO_PeriClockControl
+ * @brief   Enables or disables peripheral clock for the given GPIO port
+ *
+ * @param   pGPIOx : Base address of the GPIO port
+ * @param   EnorDi : ENABLE or DISABLE macros
+ */
+void GPIO_PeriClockControl(GPIOx_RegDef_t *pGPIOx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pGPIOx == GPIOA)      { GPIOA_CLK_EN(); }
+		else if(pGPIOx == GPIOB) { GPIOB_CLK_EN(); }
+		else if(pGPIOx == GPIOC) { GPIOC_CLK_EN(); }
+		else if(pGPIOx == GPIOD) { GPIOD_CLK_EN(); }
+		else if(pGPIOx == GPIOF) { GPIOF_CLK_EN(); }
+	}
+	else
+	{
+		if(pGPIOx == GPIOA)      { GPIOA_CLK_DIS(); }
+		else if(pGPIOx == GPIOB) { GPIOB_CLK_DIS(); }
+		else if(pGPIOx == GPIOC) { GPIOC_CLK_DIS(); }
+		else if(pGPIOx == GPIOD) { GPIOD_CLK_DIS(); }
+		else if(pGPIOx == GPIOF) { GPIOF_CLK_DIS(); }
+	}
+}
