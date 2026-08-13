35f4cc2ace6de2f9c9fe1a0990ab07688f02b76f
SayedFaisalHashimi <sayedfaisalhashimi612@gmail.com>
2026-08-13 16:06:23 +0300

    feat(gpio): implement GPIO pin initialization API
    Implement `GPIO_Init()` to configure MODER, OSPEEDR, PUPDR, OTYPER,
    and AFR registers based on `GPIO_Handle_t` settings, using a clear-then-set
    bitwise sequence and auto-enabling peripheral clocks.

--- Drivers/Src/STM32F030R8T6_gpioxx_drivers.c
+++ Drivers/Src/STM32F030R8T6_gpioxx_drivers.c
@@ -34,3 +34,49 @@ void GPIO_PeriClockControl(GPIOx_RegDef_t *pGPIOx, uint8_t EnorDi)
 		else if(pGPIOx == GPIOF) { GPIOF_CLK_DIS(); }
 	}
 }
+
+
+/**
+ * @fn      GPIO_Init
+ * @brief   Initializes the given GPIO pin according to the specified parameters
+ *
+ * @param   pGPIOHandle : Pointer to the GPIO handle structure
+ */
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+	uint32_t temp = 0;
+    uint8_t pinNumber = pGPIOHandle->GPIO_PinConfig.GPIO_PinNumber;
+
+	// Enable clock for the port just in case it wasn't done prior
+	GPIO_PeriClockControl(pGPIOHandle->pGPIOx, ENABLE);
+
+	// 1. Configure the mode of the GPIO pin
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinMode << (2 * pinNumber));
+	pGPIOHandle->pGPIOx->MODER &= ~(0x3 << (2 * pinNumber)); // Clearing
+	pGPIOHandle->pGPIOx->MODER |= temp; // Setting
+
+	// 2. Configure the speed
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinSpeed << (2 * pinNumber));
+	pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << (2 * pinNumber));
+	pGPIOHandle->pGPIOx->OSPEEDR |= temp;
+
+	// 3. Configure the pull-up/pull-down settings
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinPuPdControl << (2 * pinNumber));
+	pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << (2 * pinNumber));
+	pGPIOHandle->pGPIOx->PUPDR |= temp;
+
+	// 4. Configure the output type
+	temp = (pGPIOHandle->GPIO_PinConfig.GPIO_PinOPType << pinNumber);
+	pGPIOHandle->pGPIOx->OTYPER &= ~(0x1 << pinNumber);
+	pGPIOHandle->pGPIOx->OTYPER |= temp;
+
+	// 5. Configure alternate functionality if applicable
+	if(pGPIOHandle->GPIO_PinConfig.GPIO_PinMode == GPIO_MODE_ALTFN)
+	{
+		uint8_t temp1 = pinNumber / 8; // determines AFR[0] (low) or AFR[1] (high)
+		uint8_t temp2 = pinNumber % 8; // determines bit position within AFR
+
+		pGPIOHandle->pGPIOx->AFR[temp1] &= ~(0xF << (4 * temp2));
+		pGPIOHandle->pGPIOx->AFR[temp1] |= (pGPIOHandle->GPIO_PinConfig.GPIO_PinAltFunMode << (4 * temp2));
+	}
+}
