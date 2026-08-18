fb6a5347aceb5f4aa2cfe7135135b16c32e2e4e4
SayedFaisalHashimi <sayedfaisalhashimi612@gmail.com>
2026-08-18 13:58:35 +0300

    feat(driver): add SPI_SSOEConfig implementation for SSOE bit control
    - Set SSOE bit (1 << 2) in SPI_CR2 register when EnOrDi equals ENABLE
    - Clear SSOE bit (~(1 << 2)) in SPI_CR2 register when EnOrDi equals DISABLE
    - Enables or disables hardware NSS pin output assertion in master mode

--- Drivers/Src/STM32F0R8T_SPI.c
+++ Drivers/Src/STM32F0R8T_SPI.c
@@ -161,3 +161,16 @@ void SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
 }
 
 
+void SPI_SSOEConfig(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
+{
+    if (EnOrDi == ENABLE)
+    {
+        pSPIx->CR2 |= (1 << 2); // Set SSOE Bit
+    }
+    else
+    {
+        pSPIx->CR2 &= ~(1 << 2); // Clear SSOE Bit
+    }
+}
+
+
