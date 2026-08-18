f0d3a1bd3c7f1fe01004f20e3228c1687200de0f
SayedFaisalHashimi <sayedfaisalhashimi612@gmail.com>
2026-08-18 13:45:06 +0300

    feat(driver): add SPI_SendData driver function for polling TX
    - Poll TXE status flag prior to loading DR
    - Check CR2 DS bitfield to evaluate 8-bit vs 16-bit frame mode
    - Perform uint8_t DR pointer cast in 8-bit mode to handle Cortex-M0 FIFO packing
    - Increment buffer pointers correctly based on frame bit-width (1 byte vs 2 bytes)

--- Drivers/Src/STM32F0R8T_SPI.c
+++ Drivers/Src/STM32F0R8T_SPI.c
@@ -82,3 +82,29 @@ uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t FlagName)
     }
     return FLAG_RESET;
 }
+
+
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len)
+{
+    while (Len > 0)
+    {
+        // Wait until TXE flag is set
+        while (SPI_GetFlagStatus(pSPIx, SPI_TXE_FLAG) == FLAG_RESET);
+
+        // Check data frame size
+        if ((pSPIx->CR2 & (0xF << 8)) == (SPI_DS_16BIT << 8))
+        {
+            // 16-bit Data transfer
+            pSPIx->DR = *((uint16_t*)pTxBuffer);
+            Len -= 2;
+            pTxBuffer += 2; // Advance pointer by 2 bytes for 16-bit frame
+        }
+        else
+        {
+            // 8-bit Data transfer (Must cast to uint8_t for F0 FIFO handling)
+            *(volatile uint8_t*)&pSPIx->DR = *pTxBuffer;
+            Len--;
+            pTxBuffer++;    // Advance pointer by 1 byte for 8-bit frame
+        }
+    }
+}
