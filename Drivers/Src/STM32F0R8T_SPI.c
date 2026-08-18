588b6e3c52875fbff8fd0eea110bd6f36acd804f
SayedFaisalHashimi <sayedfaisalhashimi612@gmail.com>
2026-08-18 13:35:38 +0300

    feat(driver): add peripheral clock enable/disable logic for SPI
    - Implement SPI_PeriClockControl function in driver C file
    - Add base address comparison logic for SPI1 and SPI2 instances
    - Invoke SPIx_PCLK_EN() and SPIx_PCLK_DI() RCC macros based on ENABLE/DISABLE flag

--- Drivers/Src/STM32F0R8T_SPI.c
+++ Drivers/Src/STM32F0R8T_SPI.c
@@ -5,4 +5,18 @@
  *      Author: Sayed Faisal Hashimi
  */
 
+#include "STM32F030R8T6_SPI.h"
 
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
+{
+    if (EnOrDi == ENABLE)
+    {
+        if (pSPIx == SPI1) SPI1_PCLK_EN();
+        else if (pSPIx == SPI2) SPI2_PCLK_EN();
+    }
+    else
+    {
+        if (pSPIx == SPI1) SPI1_PCLK_DI();
+        else if (pSPIx == SPI2) SPI2_PCLK_DI();
+    }
+}
